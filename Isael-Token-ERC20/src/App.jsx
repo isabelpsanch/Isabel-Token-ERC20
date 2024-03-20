bd91ad2929cc38f183566ea275d3d248a1067308
isabelpsanch <isabelparraehs@gmail.com>
2024-03-20 14:52:33 +0100

    Añadir nuevas actualizaciones

--- Isael-Token-ERC20/src/App.jsx
+++ Isael-Token-ERC20/src/App.jsx
@@ -1,8 +1,9 @@
 import { ConnectKitProvider } from 'connectkit'
-import { WagmiConfig } from 'wagmi'
-import { AppLayout } from './components/ui/layouts'
-import { config } from './config/wagmi'
+import { WagmiConfig } from './Config/wagmi'
+import { AppLayout } from './Components/ui/layouts'
+import { config } from './config'
 import { Home } from './pages'
+
 
 function App() {
   return (
--- Isael-Token-ERC20/src/Components/ui/layouts/AppLayout.jsx
+++ Isael-Token-ERC20/src/Components/ui/layouts/AppLayout.jsx
@@ -1,1 +1,16 @@
-export { default as AppLayout } from './AppLayout';
+import { Header, Footer } from '../../index'
+import PropTypes from 'prop-types'
+
+export default function AppLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main className="min-h-[85vh]">{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired
+}
